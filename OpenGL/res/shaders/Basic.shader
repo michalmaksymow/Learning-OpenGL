d464bac80465d1d7efe1ff44abcd8b7171dc633b
Michał Maksymów <borysbrat@gmail.com>
2020-11-02 10:25:12 +0100

    Change positioning using projection matrixes

--- OpenGL/res/shaders/Basic.shader
+++ OpenGL/res/shaders/Basic.shader
@@ -31,5 +31,5 @@ uniform sampler2D u_Texture;
 void main()
 {
 	vec4 texColor = texture(u_Texture, v_TexCoord);
-   color = texColor;
+	color = texColor;
 }
--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -28,7 +28,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "OpenGL", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -51,10 +51,10 @@ int main(void)
     {
         float positions[] =
         {
-            -0.5f, -0.5f, 0.0f, 0.0f, // 0
-             0.5f, -0.5f, 1.0f, 0.0f, // 1
-             0.5f,  0.5f, 1.0f, 1.0f, // 2
-            -0.5f,  0.5f, 0.0f, 1.0f  // 3
+            100.0f, 100.0f, 0.0f, 0.0f, // 0
+            200.0f, 100.0f, 1.0f, 0.0f, // 1
+            200.0f, 200.0f, 1.0f, 1.0f, // 2
+            100.0f, 200.0f, 0.0f, 1.0f  // 3
         };
 
         uint32_t indicies[] =
@@ -74,7 +74,7 @@ int main(void)
 
         IndexBuffer ib(indicies, 6);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
