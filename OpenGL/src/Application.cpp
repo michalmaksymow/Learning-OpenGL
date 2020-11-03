6bd9c25159bad8045f041796e822325d1a2d9ca4
Michał Maksymów <borysbrat@gmail.com>
2020-11-03 10:29:52 +0100

    Render multiple objects

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -58,10 +58,10 @@ int main(void)
     {
         float positions[] =
         {
-            100.0f, 100.0f, 0.0f, 0.0f, // 0
-            200.0f, 100.0f, 1.0f, 0.0f, // 1
-            200.0f, 200.0f, 1.0f, 1.0f, // 2
-            100.0f, 200.0f, 0.0f, 1.0f  // 3
+            -50.0f, -50.0f, 0.0f, 0.0f, // 0
+            50.0f, -50.0f, 1.0f, 0.0f, // 1
+            50.0f, 50.0f, 1.0f, 1.0f, // 2
+            -50.0f, 50.0f, 0.0f, 1.0f  // 3
         };
 
         uint32_t indicies[] =
@@ -82,7 +82,7 @@ int main(void)
         IndexBuffer ib(indicies, 6);
 
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
         
 
         Shader shader("res/shaders/Basic.shader");
@@ -102,7 +102,8 @@ int main(void)
         ImGui_ImplOpenGL3_Init(glsl_version);
         ImGui::StyleColorsDark();
 
-        glm::vec3 translations(200, 200, 0);
+        glm::vec3 translationA(200, 200, 0);
+        glm::vec3 translationB(400, 200, 0);
 
         bool show_demo_window = true;
         bool show_another_window = false;
@@ -118,16 +119,26 @@ int main(void)
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translations);
-            glm::mat4 mvp = proj * view * model;
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+                shader.SetUniformMat4f("u_MVP", mvp);
+                shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+                renderer.Draw(va, ib, shader);
+            }    
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+                shader.SetUniformMat4f("u_MVP", mvp);
+                shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+                renderer.Draw(va, ib, shader);
+            }
 
-            renderer.Draw(va, ib, shader);
-            shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-            shader.SetUniformMat4f("u_MVP", mvp);
             
             {
                 ImGui::Begin("Hello, world!");
-                ImGui::SliderFloat3("Translation", &translations.x, 0.0f, 960.0f);
+                ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);
+                ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
                 ImGui::End();
             }
@@ -149,5 +160,5 @@ int main(void)
 
     glfwTerminate();
 
-    return EXIT_SUCCESS; 
+    return EXIT_SUCCESS;
 }
