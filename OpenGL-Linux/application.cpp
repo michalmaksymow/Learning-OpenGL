fa75cadecc8ae67baab3dbfba72e8da6315f8e5e
michalmaksymow <borysbrat@gmail.com>
2021-01-21 16:59:44 +0100

    Add GLEW library, render triangle

--- OpenGL-Linux/application.cpp
+++ OpenGL-Linux/application.cpp
@@ -1,8 +1,11 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
-    GLFWwindow* window;
+    GLFWwindow *window;
 
     if (!glfwInit())
         return -1;
@@ -16,9 +19,21 @@ int main(void)
 
     glfwMakeContextCurrent(window);
 
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+        std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
 
         glfwSwapBuffers(window);
 
