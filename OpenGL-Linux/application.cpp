d9cd37297a8ca9ee8ac791116c4f0a36dae9ad6d
michalmaksymow <borysbrat@gmail.com>
2021-01-22 13:14:26 +0100

    Setup glfw hints, move to 4.5 OpenGL version

--- OpenGL-Linux/application.cpp
+++ OpenGL-Linux/application.cpp
@@ -1,39 +1,69 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <cstdint>
 
 int main(void)
 {
+    
+    
     GLFWwindow *window;
 
     if (!glfwInit())
-        return -1;
+        return EXIT_FAILURE;
 
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
-        return -1;
+        return EXIT_FAILURE;
     }
 
     glfwMakeContextCurrent(window);
 
+    // Apply 4x antialiasing
+    glfwWindowHint(GLFW_SAMPLES, 4); 
+
+    // Setup OpenGL version (4.5)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+
+    // Setup for MacOS
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+
+    // Disable old OpenGL
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
+
     GLenum err = glewInit();
     if (GLEW_OK != err)
+    {
         std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+        return EXIT_FAILURE;
+    }
+    
 
     std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = 
+    {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    uint32_t bufferID;
+    glGenBuffers(1, &bufferID);
+    std::cout << bufferID << std::endl;
+    glBindBuffer(GL_ARRAY_BUFFER, bufferID);
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
     
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
 
@@ -41,5 +71,5 @@ int main(void)
     }
 
     glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
 }
